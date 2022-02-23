1a54018e2363ad165db9c8b30699962811bcba06
Vinicios Santos <95624273+vinicios-santos@users.noreply.github.com>
2022-02-23 16:59:34 -0300

    Add files via upload

---
+++ 10 - Numero repetido/script.js
@@ -0,0 +1,44 @@
+document.querySelector('.adicionar').addEventListener('click', adiciona);
+document.querySelector('.numeros').addEventListener('click', mostraNumeros);
+document.querySelector('.repetidos').addEventListener('click', confereRepetidos);
+
+let posi = 0;
+let posiRepetidos = 0;
+let vetor = new Array(posi);
+let vetorRepetidos = new Array(posiRepetidos)
+let tamanho
+let i;
+let j;
+let resp = 0;
+
+
+function adiciona() {
+    document.querySelector(".repetidos").style.display = "unset";
+    document.querySelector(".numeros").style.display = "unset";
+    let numero = document.querySelector('input').value;
+    vetor.push(numero);
+    console.log(vetor);
+    posi++
+    tamanho = posi;
+
+}
+function mostraNumeros() {
+    alert(Array(vetor))
+}
+function confereRepetidos() {
+    console.log("entrou")
+    for (i = 0; i <= tamanho; i++) {
+        for (j = i + 1; j < tamanho; j++) {
+            if (vetor[j] == vetor[i]) {
+                resp = 1;
+                vetorRepetidos.push(j);
+                posiRepetidos++
+            }
+        }
+    }
+    if (resp == 1) {
+        alert(vetorRepetidos)
+    } else {
+        alert("Não")
+    }
+}
